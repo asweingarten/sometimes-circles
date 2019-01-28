4b02906dc6c48070fa3fa1d15d72daf378bac737
asweingarten <asweingarten@gmail.com>
2019-01-27 17:21:06 -0800

    Delete dead code

--- app/Main.hs
+++ app/Main.hs
@@ -13,21 +13,17 @@ import Data.Random.Distribution.Bernoulli
 import Data.Random.Source.StdGen
 import Data.Time.Clock.POSIX
 
-import Data.List.Split
-import Control.Applicative
-import Data.Coerce
-import Data.List
 import Data.Maybe
 
 import Control.Monad.State
+
+data Brush a = Arc a
+             | None a
 
 main :: IO ()
 main = do
   seed <- round . (*1000) <$> getPOSIXTime
   let src = mkStdGen seed
-
-  -- let diagram = arc (angleDir $ 0 @@ deg) (90 @@ deg)
-  -- let diagram = sometimesCircle (0.0, 0.0) 1
 
   let arcs' = fmap (\(dir, sweep) -> arc (angleDir dir) sweep)
                    . fmap (\(dir, sweep) -> (dir @@ deg, sweep @@deg))
@@ -37,9 +33,6 @@ main = do
   let diagram = foldr atop mempty arcs'
 
   renderCairo "./out.png" (dims $ V2 300 300) $ diagram # bgFrame 1 white
-
-data Brush a = Arc a
-             | None a
 
 nom :: Brush Double -> State [Double] (Maybe (Double, Double))
 nom (None _) = do
@@ -58,52 +51,24 @@ nom (Arc d) = do
   put $ workingArc ++ [d]
   return Nothing
 
--- State s <thing I need that is obtained using s>
--- one function will be the mega computation
--- it will call other functions I had
-
-getArc :: [Brush Double] -> Maybe (Double, Double)
-getArc [] = Nothing
-getArc ((None d):pts) = Nothing
-getArc ((Arc d):pts) =
-  let (Arc final) = last pts
-   in
-    Just (d, (final - d))
-
-
-foo :: State [(Double, Double)] ()
-foo = undefined
-
 brushStrokes :: StdGen -> [Brush Double]
 brushStrokes src =
   fmap (\(d, c) ->  if c then (Arc d) else (None d))
   $ zip degrees coinFlips
     where
       degrees = [0, 0.5 .. 360]
-      (coinFlips, src') = (flip runState) src $ replicateM (length degrees) coinFlip'
+      (coinFlips, src') = (flip runState) src $ replicateM (length degrees) coinFlip
 
-coinFlip :: StdGen -> (Bool, StdGen)
-coinFlip src =
-  runState (runRVar (boolBernoulli (0.5::Double)) StdRandom) src
-
-coinFlip' :: State StdGen Bool
-coinFlip' =
+coinFlip :: State StdGen Bool
+coinFlip =
   runRVar (boolBernoulli (0.5::Double)) StdRandom
-
-circlePoints :: (Double, Double) -> Double -> [P2 Double]
-circlePoints (cx, cy) r =
-  (flip fmap) [0, 0.5 .. 360] $ p2 . \d ->
-                    let
-                      rads = d * (pi / 180)
-                      rx = r * cos (rads)
-                      ry = r * sin (rads)
-                      x = cx + rx
-                      y = cy + ry
-                    in
-                      (x, y)
 
 -- Get the points of a circle's perimeter
 -- at every point, flip a koin to decide if we connect the next point into an arc
 -- when in connecting point state, then it's biased to stay that way and vice versa
 --  (monad?)
 -- do this multiple times with circles whose origins drift along a path (start with diagonal)
+--
+-- State s <thing I need that is obtained using s>
+-- one function will be the mega computation
+-- it will call other functions I had
